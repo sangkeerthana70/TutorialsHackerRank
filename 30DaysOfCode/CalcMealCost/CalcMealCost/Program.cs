6dd1e20c6ea74191d15573df2716ab1f41a8a931
sangkeerthana70 <sangkeerthana70@gmail.com>
2018-11-14 13:36:25 -0500

    Day2-Operators in Hackkerank

---
+++ 30DaysOfCode/CalcMealCost/CalcMealCost/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcMealCost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double meal_cost;
+            int tip_percent;
+            int tax_percent;
+            var totalCost = Solve(12.00, 20, 8);
+            Console.WriteLine("Total cost of meal: " + totalCost);
+        }
+
+        static double Solve(double meal_cost, int tip_percent, int tax_percent)
+        {
+            double tip;
+            double tax;
+            double total_cost;
+            tip = meal_cost * tip_percent / 100;
+            Console.WriteLine("Tip: " + tip);
+            tax = meal_cost * tax_percent / 100;
+            Console.WriteLine("Tax: " + tax);
+
+            total_cost = Math.Round(meal_cost + tip + tax);
+            Console.WriteLine("total cost: " + total_cost);
+            return total_cost;
+
+        }
+
+    }
+}
---
+++ CloudyDay/CloudyDay/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudyDay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] population = new int[] { 1000, 500, 750, 2000, 2500 };
+            int[] townLoc = new int[] { 10, 30, 50, 60, 80 };
+            int[] cloudLoc = new int[] { 15, 45, 70 };
+            int[] cloudRange = new int[] { 6, 10, 5 };
+
+            var result = GetMaxPopulation(population, townLoc, cloudLoc, cloudRange);
+            Console.WriteLine(result);
+        }
+
+        private static object GetMaxPopulation(int[] population, int[] townLoc, int[] cloudLoc, int[] cloudRange)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
