8f78bee16e5ac35ab7770f64f2fecafe870ebb9f
sangkeerthana70 <sangkeerthana70@gmail.com>
2018-11-15 12:30:36 -0500

    Conditionals exercise

--- 30DaysOfCode/DataTypes/DataTypes/Program.cs
+++ 30DaysOfCode/DataTypes/DataTypes/Program.cs
@@ -16,9 +16,15 @@ namespace DataTypes
 
             // Declare second integer, double, and String variables.
             // Read and save an integer, double, and String to your variables.
+            
             int i2 = Convert.ToInt32(Console.ReadLine());
+            i2 = 8;
+            
             double d2 = Convert.ToDouble(Console.ReadLine());
+            d2 = 4.0;
             string s2 = Console.ReadLine();
+            s2 = "is the best place to learn coding!";
+
             // Print the sum of both integer variables on a new line.
             Console.WriteLine(i + i2);
             // Print the sum of the double variables on a new line.
