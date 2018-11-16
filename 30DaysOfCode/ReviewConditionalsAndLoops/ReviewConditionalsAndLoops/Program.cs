9841a078b3aedcfa3e550688f4df2b225f9d4a52
sangkeerthana70 <sangkeerthana70@gmail.com>
2018-11-16 11:05:21 -0500

    Basic Programming concepts

---
+++ 30DaysOfCode/ReviewConditionalsAndLoops/ReviewConditionalsAndLoops/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReviewConditionalsAndLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string S1 = "Hacker";
+            string S2 = "Rank";
+            string even1 = "";
+            string odd1 = "";
+            string even2 = "";
+            string odd2 = "";
+            //Loop through string S1
+            for (int i = 0; i < S1.Length; i++)
+            {
+                //Console.WriteLine( i + "%2: " + i%2);
+                if (i % 2 == 0)
+                {
+
+                    //Console.WriteLine(S1[i] + " is even");
+                    even1 += S1[i];
+                    
+                }
+                else
+                {
+                    //Console.WriteLine(S1[i] + " is odd");
+                    odd1 += S1[i];
+                }
+            }
+
+            for (int i = 0; i < S2.Length; i++)
+            {
+                //Console.WriteLine(i + "%2: " + i % 2);
+                if (i % 2 == 0)
+                {
+
+                    //Console.WriteLine(S2[i] + " is even");
+                    even2 += S2[i];
+
+                }
+                else
+                {
+                    //Console.WriteLine(S2[i] + " is odd");
+                    odd2 += S2[i];
+                }
+            }
+
+            Console.WriteLine(even1 +  " " + odd1);
+            Console.WriteLine(even2 + " " + odd2);
+        }
+    }
+}
