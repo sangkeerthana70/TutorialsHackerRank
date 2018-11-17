25fa980ba6cc8252634d867b077de27e2eb2122f
sangkeerthana70 <sangkeerthana70@gmail.com>
2018-11-17 17:28:22 -0500

    Arrays sort and reverse functions

---
+++ 30DaysOfCode/Arrays/Arrays/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr1 = new int[] { 1, 4, 3, 2};
+            int[] arr2 = new int[4];
+
+            // Copy one array into the other
+            Array.Copy(arr1, arr2, 4);
+            Console.WriteLine("--- Target array ---");
+            foreach (int value in arr2)
+            {
+                Console.WriteLine(value);
+            }
+            Console.WriteLine();
+            //Reverse an array
+            // Reverses the sort of the values of the Array.
+            Array.Reverse(arr1);
+
+            foreach( int i in arr1)
+            {
+                Console.Write(i + " ");
+            }
+
+        }
+    }
+}
