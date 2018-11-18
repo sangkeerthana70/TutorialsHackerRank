03771520b85abdb9fe4e1c01c6dce73104a46513
sangkeerthana70 <sangkeerthana70@gmail.com>
2018-11-18 00:43:07 -0500

    Inheritance explained

---
+++ 30DaysOfCode/Inheritance/Inheritance/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using System.Linq;
+
+namespace Inheritance
+{
+    class Person
+    {
+ 
+        protected string firstName;
+        protected string lastName;
+        protected int id;
+
+        public Person() { }
+        public Person(string firstName, string lastName, int identification)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.id = identification;
+        }
+        public void printPerson()
+        {
+            Console.WriteLine("Name: " + lastName + ", " + firstName + "\nID: " + id);
+        }
+    }
+}
+
---
+++ 30DaysOfCode/Inheritance/Inheritance/Student.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Student : Person
+    {
+        private int[] testScores;
+
+
+        // Write your constructor here
+        public Student() { }
+        public Student(string firstName, string lastName, int identification, int[] scores)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.id = identification;
+            this.testScores = scores;
+        }
+
+        /*	
+        *   Method Name: Calculate
+        *   Return: A character denoting the grade.
+        */
+        // Write your method here
+
+        public char Calculate()
+        {
+            int total = 0;
+            int average = 0;
+            char grade;
+            for (int i = 0; i < testScores.Length; i++)
+            {
+                total += testScores[i];
+            }
+
+            average = total / testScores.Length;
+            if ((average >= 90) && (average <= 100))
+            {
+                grade = 'O';
+            }
+            else if (average >= 80 && average < 90)
+            {
+                grade = 'E';
+            }
+            else if (average >= 70 && average < 80)
+            {
+                grade = 'A';
+            }
+            else if (average >= 55 && average < 70)
+            {
+                grade = 'P';
+            }
+            else if (average >= 40 && average < 55)
+            {
+                grade = 'D';
+            }
+            else
+            {
+                grade = 'T';
+            }
+            return grade;
+        }
+
+
+    }
+}
