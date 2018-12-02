64d801a01cdd04158ab36fc6183ab0572c052570
sangkeerthana70 <sangkeerthana70@gmail.com>
2018-12-01 21:24:50 -0500

    A very big sum

---
+++ Algorithms/AVeryBigSum/AVeryBigSum/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AVeryBigSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 5;
+            long[] ar = new long[] { 1000000001, 1000000002, 1000000003, 1000000004, 1000000005 };
+            long sum = aVeryBigSum(ar);
+            
+        }
+
+        // Complete the aVeryBigSum function below.
+        static long aVeryBigSum(long[] ar)
+        {
+            long sum = 0;
+            for (int i = 0; i < ar.Length; i++)
+            {
+                sum += ar[i];
+            }
+            Console.WriteLine(sum);
+            return sum;
+
+
+        }
+    }
+}
