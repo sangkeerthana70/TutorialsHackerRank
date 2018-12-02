5d29a2831c29441db5bbfe9b3ff3216158d8db7c
sangkeerthana70 <sangkeerthana70@gmail.com>
2018-12-02 12:52:38 -0500

    Little bug in code to be fixed

---
+++ Algorithms/StairCase/StairCase/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StairCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sizeOfStairCase = 6;
+            staircase(sizeOfStairCase);
+        }
+
+        // Complete the staircase function below.
+        static void staircase(int n)
+        {
+
+            for (int i = 0; i < (n); i++)
+            {
+                Console.WriteLine(" ");
+                for (int j = 0; j < n - i - 2; j++)
+                {
+
+                    Console.Write(" ");
+                }
+                for (int k = 0; k <= i; k++)
+                {
+                    Console.Write("#");
+                }
+                //Console.WriteLine();
+            }
+            //Console.WriteLine();
+
+            //for (int i = 0; i < n; i++)
+            //{    
+            //    for (int j = 0; j < n; j++)
+            //    {
+            //        Console.Write("#");
+            //    }               
+            //    Console.WriteLine();
+            //}
+            //Console.WriteLine();
+ 
+
+        }
+
+    }
+}
