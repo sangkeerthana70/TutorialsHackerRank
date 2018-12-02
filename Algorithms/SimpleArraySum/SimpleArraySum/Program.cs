5aa4218ed5eafc0ffd966d3e94069f8641b937ba
sangkeerthana70 <sangkeerthana70@gmail.com>
2018-12-01 21:15:38 -0500

    Simple array sum

--- Algorithms/SimpleArraySum/SimpleArraySum/Program.cs
+++ Algorithms/SimpleArraySum/SimpleArraySum/Program.cs
@@ -10,6 +10,23 @@ namespace SimpleArraySum
     {
         static void Main(string[] args)
         {
+            int sizeOfArr = 6;
+            int[] ar = new int[] { 1, 2, 3, 4, 10, 11 };
+
+            int sum = simpleArraySum(ar);
+
+        }
+
+        static int simpleArraySum(int[] ar)
+        {
+            int sum = 0;
+            for(int i = 0; i < ar.Length; i++)
+            {
+                sum += ar[i];
+               
+            }
+            Console.WriteLine(sum);
+            return sum;
         }
     }
 }
