0489d73dae2a078370a06db63732930240041f1f
sangkeerthana70 <sangkeerthana70@gmail.com>
2018-12-02 12:51:14 -0500

    Solve me first

---
+++ Algorithms/SolveMeFirst/SolveMeFirst/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolveMeFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 2;
+            int b = 3;
+            int sum = solveMeFirst(a, b);
+        }
+
+        static int solveMeFirst(int a, int b)
+        {
+            // Hint: Type return a+b; below  
+            int sum = a + b;
+            Console.WriteLine(sum);
+            return sum;           
+        }
+
+    }
+}
