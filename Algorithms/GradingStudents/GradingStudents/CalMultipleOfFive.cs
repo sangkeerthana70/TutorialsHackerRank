651e96480da5aa81fb02699f1f24e8980248083b
sangkeerthana70 <sangkeerthana70@gmail.com>
2018-12-12 23:21:01 -0500

    Grading students

---
+++ Algorithms/GradingStudents/GradingStudents/CalMultipleOfFive.cs
@@ -0,0 +1,8 @@
+reference object, instance Vs class
+
+var quotient = grades[i] / 5;
+                        Console.WriteLine(quotient);
+                        var nextFifth = (quotient + 1) * 5;
+                        Console.WriteLine(nextFifth);
+                        finalGrade = nextFifth;
+                        Console.WriteLine(finalGrade);
