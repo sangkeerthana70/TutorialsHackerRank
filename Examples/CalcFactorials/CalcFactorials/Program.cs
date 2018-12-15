f14dfb89c4e023e97dcd3d6925a3e846e076289a
sangkeerthana70 <sangkeerthana70@gmail.com>
2018-12-14 23:57:10 -0500

    Calculate factors of a number

---
+++ Examples/CalcFactorials/CalcFactorials/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcFactorials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 12;
+            int result = getFactors(num);
+        }
+
+        public static int  getFactors(int num)
+        {// returns all factorials.
+            //var factors = [];
+            int[] factors = new int[] { };
+            for (var i = 1; i <= num; i++)
+            {
+                if (num % i == 0)
+                {
+                    factors.push(i);
+                }
+            }
+            return factors;
+        }
+    }
+}
