b13b2625dd593c53fc2feb3e07ff548da08f555d
sangkeerthana70 <sangkeerthana70@gmail.com>
2018-12-18 11:16:43 -0500

    Calc odd or even number

---
+++ Examples/CalculateOddOrEvenNumber/CalculateOddOrEvenNumber/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculateOddOrEvenNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 2; i <= 100; i++)
+            {
+                if(i % 2 == 0)
+                {
+                    Console.WriteLine(i + " is even");
+                }
+                else
+                {
+                    Console.WriteLine(i + " is odd");
+                }
+            }
+        }
+    }
+}
