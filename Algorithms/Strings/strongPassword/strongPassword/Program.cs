3546e8de641f92eb4724555498c7d398a0e6a0c1
sangkeerthana70 <sangkeerthana70@gmail.com>
2018-12-20 12:19:55 -0500

    working on strong password algo

--- Algorithms/Strings/strongPassword/strongPassword/Program.cs
+++ Algorithms/Strings/strongPassword/strongPassword/Program.cs
@@ -13,37 +13,71 @@ namespace strongPassword
             int n = 11;
             string password = "#HackerRank";
             int answer = minimumNumber(n, password);
+            Console.WriteLine("Answer: " + answer);
         }
 
         // Complete the minimumNumber function below.
         static int minimumNumber(int n,string password)
         {
-            
+
             // Return the minimum number of characters to make the password strong
+            int uppChar = 0;
+            int lowerChar = 0;
+            int specialChar = 0;
+            int numChar = 0;
+            int passwordCount = password.Length;
+            string special_characters = "!@#$%^&*()-+";
+            string numbers = "0123456789";
+
             for (int i = 0; i < password.Length; i++)
             {
-                string special_characters = "!@#$%^&*()-+";
+                
                 if (password.Length == n)
                 {
 
                     if (char.IsUpper(password[i]))
                     {
+                        uppChar += 1;
                         Console.WriteLine("has one upperCharacter");
                     }
                     if (char.IsLower(password[i]))
                     {
-                        Console.WriteLine(("has one lower");
+                        lowerChar += 1;
+                        Console.WriteLine("has one lower");
                     }
                     
                     for( int c = 0; c < special_characters.Length; c++)
                     {
                         if (password[i] == special_characters[c])
                         {
+                            specialChar += 1;
                             Console.WriteLine("has a special character");
                         }
                     }
+
+                    for ( int num = 0; num < numbers.Length; num++)
+                    {
+                        if(password[i] == numbers[num])
+                        {
+                            numChar += 1;
+                        }
+                    }
+                  
                 }
+
             }
+            if ((uppChar == 1 && lowerChar == 1 && specialChar == 1 && numChar == 1) && passwordCount == 8)
+            {
+                Console.WriteLine("Has all requirements");
+                return passwordCount;
+                
+            }
+            else
+            {
+                Console.WriteLine("Not met all requirements");
+                
+            }
+            return passwordCount;
 
 
         }
