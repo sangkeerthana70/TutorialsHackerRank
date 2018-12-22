555762746f4258143791335e0e939866859e5205
sangkeerthana70 <sangkeerthana70@gmail.com>
2018-12-22 13:41:08 -0500

    Find the median of numbers in an array

---
+++ Examples/calcMedianOfNumbersInArray/calcMedianOfNumbersInArray/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calcMedianOfNumbersInArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 12, 3, 5 };
+
+            int[] arr2 = new int[] { };
+            
+             Array.Sort(arr);
+            
+           
+            Console.WriteLine("After sorting the elements in arr: ");
+            foreach(int sortedItem in arr)
+            {               
+                Console.Write(sortedItem);
+                Console.WriteLine();
+            }
+            //if length is even 
+            //if length is odd
+        }
+    }
+}
