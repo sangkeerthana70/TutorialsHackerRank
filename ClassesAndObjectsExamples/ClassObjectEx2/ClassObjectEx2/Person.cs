50abb4a52c4ce980569fba994b8d7ed189a2b648
sangkeerthana70 <sangkeerthana70@gmail.com>
2018-12-23 13:19:08 -0500

    Example 2 of instantiating a class

---
+++ ClassesAndObjectsExamples/ClassObjectEx2/ClassObjectEx2/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassObjectEx2
+{
+    public class Person
+    {
+        public string Name { get; set; } 
+        public int Age { get; set; }
+        public string Profession { get; set; }
+
+        public Person()
+        {
+
+        }
+
+        public void ShowDetails()
+        {
+            Console.Write("My name is: " + this.Name );
+            Console.Write(" And I am: " +  this.Age + " years old");
+            Console.Write(" I am a " + this.Profession);
+            Console.WriteLine();
+        }
+    }
+}
---
+++ ClassesAndObjectsExamples/ClassObjectEx2/ClassObjectEx2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassObjectEx2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Name : ");
+            string name = Console.ReadLine();
+            Console.Write("Age : ");
+            int age = Int32.Parse(Console.ReadLine());
+            Console.Write("Profession : ");
+            string profession = Console.ReadLine();
+            // Person is a class with three properties
+            // Here we are instantiating a person object and pass values to its constructor
+
+            Person Anu = new Person();
+            Anu.Name = name;
+            Anu.Age = age;
+            Anu.Profession = profession;
+            //call the method inside Person class
+            Anu.ShowDetails();
+            //take input for another instance of another class
+            Console.Write("Name : ");
+            name = Console.ReadLine();
+            Console.Write("Age : ");
+            age = Int32.Parse(Console.ReadLine());
+            Console.Write("Profession : ");
+            profession = Console.ReadLine();
+
+            Person Keethu = new Person();
+            Keethu.Name = name;
+            Keethu.Age = age;
+            Keethu.Profession = profession;
+            Keethu.ShowDetails();
+
+
+        }
+    }
+}
