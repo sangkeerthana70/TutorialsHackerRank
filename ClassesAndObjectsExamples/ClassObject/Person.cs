3ba953170736b5f874b4c0ebe335b510a85859d3
sangkeerthana70 <sangkeerthana70@gmail.com>
2018-12-23 13:14:55 -0500

    Classes and instances Example1

---
+++ ClassesAndObjectsExamples/ClassObject/Person.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassObject
+{
+    public class Person
+    {
+        string Name;
+        int Age;
+        string Profession;
+
+        //public Person()
+        //{
+        //}
+
+        public Person(string N, int A, string P)
+        {
+            this.Name = N;
+            this.Age = A;
+            this.Profession = P;
+        }
+
+        
+
+        public void showDetails()
+        {
+            Console.Write("My name is: " + Name);
+            Console.Write(" And I am: " + Age + " years old");
+            Console.Write(" I am a " + Profession);
+            Console.WriteLine();
+        }
+
+    }
+}
---
+++ ClassesAndObjectsExamples/ClassObject/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassObject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Name : ");
+            string name = Console.ReadLine();
+            Console.Write("Age : ");
+            int age = Int32.Parse(Console.ReadLine());
+            Console.Write("Profession : ");
+            string profession = Console.ReadLine();
+            // Person is a class with three properties
+            // Here we are instantiating a person object and pass values to its constructor
+            Person Anu = new Person(name, age, profession);
+            //person.showDetails();
+            Console.Write("Name : ");
+            name = Console.ReadLine();
+            Console.Write("Age : ");
+            age = Int32.Parse(Console.ReadLine());
+            Console.Write("Profession : ");
+            profession = Console.ReadLine();
+
+            Person Senthil = new Person(name, age, profession);
+            Anu.showDetails();
+            Senthil.showDetails();
+
+
+           
+
+
+
+        }
+    }
+}
