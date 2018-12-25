437b3f6441ac046efebc95bae8b6936f654f2c7e
sangkeerthana70 <sangkeerthana70@gmail.com>
2018-12-24 23:45:41 -0500

    Algo for caesar cipher w.i.p

---
+++ Algorithms/Strings/CaesarCipher/CaesarCipher/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaesarCipher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "middle - Outz";
+            int k = 2;
+            string result = caesarCipher(s, k);
+        }
+
+        // Complete the caesarCipher function below.
+        static string caesarCipher(string s, int k)
+        {
+            char ch = ' ';
+            int number = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (char.IsLetter(s[i]))
+                {
+                    //byte[] asciiBytes = Encoding.ASCII.GetBytes(s[i]);
+                    //foreach (byte b in System.Text.Encoding.UTF8.GetBytes(s.ToCharArray()))
+                    // Console.Write(b.ToString());
+                    ch = s[i];
+                    number = (int)ch;
+                    Console.WriteLine(number);
+
+                }
+            }
+            return "";
+
+        }
+
+    }
+}
