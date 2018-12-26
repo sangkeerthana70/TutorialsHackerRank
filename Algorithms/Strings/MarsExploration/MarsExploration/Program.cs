48677075cc7995085b05efc3ead7a5504eb72254
sangkeerthana70 <sangkeerthana70@gmail.com>
2018-12-25 22:36:18 -0500

    Mars Exploration Initial commit

---
+++ Algorithms/Strings/MarsExploration/MarsExploration/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsExploration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "SOSSOSSOS";
+            int result = marsExploration(s);
+        }
+
+        // Complete the marsExploration function below.
+        static int marsExploration(string s)
+        {
+            for(int i = 0; i < s.Length; i++)
+            {
+                
+            }
+            return 0;
+
+
+        }
+    }
+}
