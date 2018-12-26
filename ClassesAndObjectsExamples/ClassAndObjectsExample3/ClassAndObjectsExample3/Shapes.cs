7beb376218f7e97aee7b6b1e817838b50b843abf
sangkeerthana70 <sangkeerthana70@gmail.com>
2018-12-26 14:56:47 -0500

    Polymorphism - Program.cs adds both shapes into a List of Type Box and item.About() method takes two form of displaying the About method

---
+++ ClassesAndObjectsExamples/ClassAndObjectsExample3/ClassAndObjectsExample3/Shapes.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndObjectsExample3
+{
+    public class Shapes
+    {
+        public Shapes()
+        {
+
+        }
+
+        public void About()
+        {
+            Console.WriteLine("I am a shape");
+        }
+    }
+}
