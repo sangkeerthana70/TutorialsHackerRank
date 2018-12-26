091674e4bc972758ee6a284589239a1f685ae1a2
sangkeerthana70 <sangkeerthana70@gmail.com>
2018-12-26 13:19:58 -0500

    More examples on Classes and Objects

---
+++ ClassesAndObjectsExamples/ClassAndObjectsExample3/ClassAndObjectsExample3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndObjectsExample3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Square square1 = new Square(8);
+            //square1.Area(8);
+            //square1.Perimeter(8);
+            Square square2 = new Square(12);
+            Square square3 = new Square(4);
+
+            List <Square> squares = new List <Square> ();
+            squares.Add(square1);
+            squares.Add(square2);
+            squares.Add(square3);
+            foreach(Square item in squares)
+            {
+                item.About();
+            }
+        }
+    }
+}
---
+++ ClassesAndObjectsExamples/ClassAndObjectsExample3/ClassAndObjectsExample3/Square.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndObjectsExample3
+{
+    public class Square
+    {
+        public int side;
+
+        public Square(int s){
+            this.side = s;
+        }
+
+        public int Area(int side)
+        {
+            int area = side * side;
+            //Console.WriteLine(area);
+            return area;
+        }
+
+        public int Perimeter(int side)
+        {
+            int perimeter = side * 4;
+            //Console.WriteLine(perimeter);
+            return perimeter;
+        }
+
+        public void About()
+        {
+            Console.Write("I am a square ");
+            Console.Write(" With an area of : " + this.Area(side));
+            Console.Write(" And a perimeter of : " + this.Perimeter(side));
+            Console.WriteLine();
+
+        }
+    }
+}
