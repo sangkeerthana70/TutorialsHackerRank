e6ff446d001029614b50a63ed8aa3b8d87c6af03
sangkeerthana70 <sangkeerthana70@gmail.com>
2018-12-26 14:50:54 -0500

    Class Rectangle and Shape inherits the base Shapes class

--- ClassesAndObjectsExamples/ClassAndObjectsExample3/ClassAndObjectsExample3/Program.cs
+++ ClassesAndObjectsExamples/ClassAndObjectsExample3/ClassAndObjectsExample3/Program.cs
@@ -16,14 +16,53 @@ namespace ClassAndObjectsExample3
             Square square2 = new Square(12);
             Square square3 = new Square(4);
 
-            List <Square> squares = new List <Square> ();
-            squares.Add(square1);
-            squares.Add(square2);
-            squares.Add(square3);
-            foreach(Square item in squares)
+            //List <Square> squares = new List <Square> ();
+            //squares.Add(square1);
+            //squares.Add(square2);
+            //squares.Add(square3);
+            //foreach(Square item in squares)
+            //{
+            //    item.About();
+            //}
+
+            //----------------------------------------------------------
+            //instantiate rectangle class into many objects
+
+            Rectangle rectangleA = new Rectangle(2, 4);
+            rectangleA.About();
+            Rectangle rectangleB = new Rectangle(6, 8);
+            rectangleB.About();
+            Rectangle rectangleC = new Rectangle(10, 12);
+
+            //List<Rectangle> rectangles = new List<Rectangle>();
+            //rectangles.Add(rectangleA);
+            //rectangles.Add(rectangleB);
+            //rectangles.Add(rectangleC);
+
+            //foreach(Rectangle item in rectangles)
+            //{
+            //    item.About();
+            //}
+
+
+            // Create a new List called Box and add squares and rectangles object in it
+
+            List<Shapes> Box = new List<Shapes>();
+            Box.Add(square1);
+            Box.Add(square2);
+            Box.Add(square3);
+
+            Box.Add(rectangleA);
+            Box.Add(rectangleB);
+            Box.Add(rectangleC);
+
+            foreach(Shapes item in Box)
             {
                 item.About();
             }
+
+            
+            
         }
     }
 }
