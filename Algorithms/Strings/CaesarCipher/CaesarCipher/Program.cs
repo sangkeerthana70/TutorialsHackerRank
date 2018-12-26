31f2713435f616f26ec94500b6151e3ce16f8cb5
sangkeerthana70 <sangkeerthana70@gmail.com>
2018-12-25 22:18:15 -0500

    Passed all test cases

--- Algorithms/Strings/CaesarCipher/CaesarCipher/Program.cs
+++ Algorithms/Strings/CaesarCipher/CaesarCipher/Program.cs
@@ -13,7 +13,7 @@ namespace CaesarCipher
             string s = "middle - Outz";
             int k = 2;
             string result = caesarCipher(s, k);
-            Console.WriteLine("result : " + result);
+            Console.WriteLine(result);
         }
 
         // Complete the caesarCipher function below.
@@ -23,7 +23,7 @@ namespace CaesarCipher
             int number = 0;
             int alphaIndex = 0;
             int result = 0;
-            string cipherString = " ";
+            string cipherString = "";
             for (int i = 0; i < s.Length; i++)
             {
                 if (char.IsLetter(s[i]))
@@ -31,27 +31,27 @@ namespace CaesarCipher
                     
                     ch = s[i];
                     number = (int)ch;
-                    Console.WriteLine("Char to int: " + number);
+                    //Console.WriteLine("Char to int: " + number);
 
                     if (Char.IsUpper(s[i]))
                     {
                         alphaIndex = number - 'A';
-                        Console.WriteLine("alphaIndex: " + alphaIndex);
+                        //Console.WriteLine("alphaIndex: " + alphaIndex);
                         result = ((alphaIndex + k) % 26) + 'A';
-                        Console.WriteLine("Result: " + result);
+                        //Console.WriteLine("Result: " + result);
                         ch = (char)result;
-                        Console.WriteLine("char: " + ch);
+                        //Console.WriteLine("char: " + ch);
                        
 
                     }
                     else
                     {
                         alphaIndex = number - 'a';
-                        Console.WriteLine("alphaIndex: " + alphaIndex);
+                        //Console.WriteLine("alphaIndex: " + alphaIndex);
                         result = ((alphaIndex + k) % 26) + 'a';
-                        Console.WriteLine("result: " + result);
+                        //Console.WriteLine("result: " + result);
                         ch = (char)result;
-                        Console.WriteLine("char: " + ch);
+                        //Console.WriteLine("char: " + ch);
                         
                     }
  
@@ -60,12 +60,13 @@ namespace CaesarCipher
                 {
                     ch = s[i];
                     
-                    Console.WriteLine(ch);
+                    //Console.WriteLine(ch);
                 }
+                cipherString += ch.ToString();
+
+                //Console.Write("cipherString: " + cipherString);
+                //Console.WriteLine();
             }
-            cipherString += ch.ToString();
-            Console.Write("cipherString: " + cipherString);
-            Console.WriteLine();
             return cipherString;
 
         }
