e9aaae81f074136ce3ecf993c45feb18f3f06227
sangkeerthana70 <sangkeerthana70@gmail.com>
2018-12-28 00:08:33 -0500

    Mars Exploration solved

--- Algorithms/Strings/MarsExploration/MarsExploration/Program.cs
+++ Algorithms/Strings/MarsExploration/MarsExploration/Program.cs
@@ -10,18 +10,66 @@ namespace MarsExploration
     {
         static void Main(string[] args)
         {
-            string s = "SOSSOSSOS";
+            string s = "SOSSOT";
             int result = marsExploration(s);
         }
 
         // Complete the marsExploration function below.
         static int marsExploration(string s)
         {
-            for(int i = 0; i < s.Length; i++)
+            //string expectedSignal = "SOS";
+            //int messagesSent = s.Length / 3;
+            //StringBuilder replicateExpectedSignal = new StringBuilder();
+            int misMatch = 0;
+            char oddChar = 'O';
+            char evenChar = 'S';
+            
+
+            for (int i = 0; i < s.Length; i++)
             {
-                
+
+                int j = i % 3;
+                // s[0] = 'S'
+                if ((j == 0) && (s[i] != 'S')) misMatch++;
+                // s[1] = 'O'
+                if ((j == 1) && (s[i] != 'O')) misMatch++;
+                // s[2] = 'S'
+                if ((j == 2) && (s[i] != 'S')) misMatch++;
+
+                //if ((j == 0) && (s[i] != 'S'))
+                //{
+                //    Console.WriteLine("i : " + i);
+                //    //Console.WriteLine("even char: " + i % 2);
+                //    Console.WriteLine("s[i] : " + s[i]);
+                //    if(s[i] != evenChar)
+                //    {
+                        
+                //        misMatch += 1;
+                //    }
+                    
+                //}
+                //else if ((j == 1) && (s[i] != 'J'))
+                //{
+                //    Console.WriteLine("i : " + i);
+                //    //Console.WriteLine("odd char: " + i % 2);
+                //    Console.WriteLine("s[i] : " + s[i]);
+                //    if (s[i] != oddChar)
+                //    {
+                        
+                //        misMatch += 1;
+                //    }
+                    
+                //}
+                //else
+                //{
+                //    misMatch = 0;
+                //}
+                Console.WriteLine("Mismatch = " + misMatch);
+
             }
-            return 0;
+            
+            return misMatch;
+
 
 
         }
