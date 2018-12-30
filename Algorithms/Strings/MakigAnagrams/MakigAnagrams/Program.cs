0d683bbda1b307cedc1e81a0030b70e4294fc588
sangkeerthana70 <sangkeerthana70@gmail.com>
2018-12-29 21:16:33 -0500

    Initial commit

---
+++ Algorithms/Strings/MakigAnagrams/MakigAnagrams/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MakigAnagrams
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "cde";
+            string s2 = "abc";
+            int result = makingAnagrams(s1, s2);
+
+        }
+
+        // two strings are considered to be anagrams of each other if the first string's letters can be 
+        // rearranged to form the second string. In other words, both strings must contain the same exact
+        //letters in the same exact frequency. 
+
+
+        // Complete the makingAnagrams function below.
+        static int makingAnagrams(string s1, string s2)
+        {
+
+
+
+
+        }
+    }
+}
