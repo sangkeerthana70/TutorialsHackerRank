e7f20c5f91a0145d467040e24fda68c55c565c23
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-01-03 22:59:55 -0500

    Find the first non-repeating character in a string

---
+++ AlgorithmCodingChallenge/FindNonRepeatingCharInString/FindNonRepeatingCharInString/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindNonRepeatingCharInString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string s = "abcabd";
+
+            char result = FindNonRepeatChar(s);
+            Console.WriteLine("result " + result);
+
+        }
+
+        
+
+
+        public static char FindNonRepeatChar(string s)
+        {
+            char letter;
+            bool seen;
+            for (int i = 0; i < s.Length; i++)
+            {
+               
+                seen = false;
+                letter = s[i];
+
+                for (int j = i+1; j < s.Length; j++)
+                {
+                    if (s[j] == letter)
+                    {
+                        seen = true;
+                        break;
+                    }
+                }
+
+                if (!seen) return letter;
+               
+            }
+            return '_';
+
+        }
+    }
+}
