d8fec9e2158f87e671f3ff3125c6ec6bba41d2e8
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-01-07 12:14:42 -0500

    Print display board using for loops

---
+++ AlgorithmCodingChallenge/DisplayBoard/DisplayBoard/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DisplayBoard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //for(int i = 0; i < 3; i++)
+            //{
+            //     Console.Write(" ");
+            //     Console.WriteLine("| |");
+            //     Console.WriteLine("-+-+-");
+            //}
+
+            int rows = 2;
+            
+
+            for(int i = 0; i < rows; i++)
+            {
+                Console.Write(" "); 
+                Console.WriteLine("| |");
+                for(int j = 0; j < rows - 1; j++)
+                {
+                    //Console.WriteLine();
+                    Console.WriteLine("-+-+-");
+                }
+                
+
+            }
+            
+            Console.WriteLine(" | | ");
+
+
+        }
+
+        
+
+               
+           
+           
+
+
+        
+    }
+}
