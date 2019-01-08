7eefceb5454d716e0925482fede42aac626bc073
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-01-07 23:44:40 -0500

    Create a  class to print tic tac toe

---
+++ AlgorithmCodingChallenge/DisplayBoard/DisplayBoard/PrintTicTacToeApproach2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DisplayBoard
+{
+    public class PrintTicTacToeApproach2
+    {       
+        int rows;
+        int columns;
+        public PrintTicTacToeApproach2(int r, int c)
+        {
+            this.rows = r;
+            this.columns = c;
+        }
+
+
+        public void printRows()
+        {       
+                Console.WriteLine(" | | ");
+        }
+        public void printColumns()
+        {
+                Console.WriteLine("-+-+-");
+        }
+    }
+}
