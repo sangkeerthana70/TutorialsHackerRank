d7e0e73583f64097324ddd3f24b4b981e809ecc6
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-01-08 15:08:33 -0500

    Hashtables

---
+++ Algorithms/Strings/Pangrams/Pangrams/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pangrams
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable pangrams = new Hashtable();
+            pangrams.Add(1, "I");
+            pangrams.Add(2, "am");
+            pangrams.Add(3, "learning");
+            pangrams.Add(4, "about");
+            pangrams.Add(5, "hash tables");
+
+            // the keys property of Hashtable gets an ICollection containing the keys in the Hashtable.
+            ICollection key = pangrams.Keys;
+            foreach (int val in key)
+            {
+                // values Gets an ICollection containing the values in the Hashtable.
+                Console.WriteLine(val + " " + pangrams[val]);
+            }
+
+
+        }
+    }
+
+    
+}
