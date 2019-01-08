c6c7e42526b919feb1d40d095f929a8eb64d45d6
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-01-07 22:38:33 -0500

    testing out with more characters inside the board

--- AlgorithmCodingChallenge/DisplayBoard/DisplayBoard/Program.cs
+++ AlgorithmCodingChallenge/DisplayBoard/DisplayBoard/Program.cs
@@ -18,25 +18,49 @@ namespace DrawBoard
             //     Console.WriteLine("-+-+-");
             //}
 
+
+            // Program to print 5 rows and 5 columns of tic-tac-toe board
+
             int rows = 2;
             
 
             for(int i = 0; i < rows; i++)
             {
-                Console.Write(" "); 
-                Console.WriteLine("| |" + " ");
+                Console.Write("o"); 
+                Console.WriteLine("|x|" + "o");
                 
                 for(int j = 0; j < rows - 1; j++)
                 {
                     
                     Console.WriteLine("-+-+-");
                 }
-                
-
             }
             // print the last row out of the loop
-            Console.WriteLine(" | | ");
+            Console.WriteLine("o|x|o");
 
+           
+            PrintTicTacToeBoard();
+            
+            
+        }
+
+        public static void PrintTicTacToeBoard()
+        {
+            Console.WriteLine();
+            int height = 2;
+            for(int i = 0; i < height; i++)
+            {
+                
+                Console.WriteLine("o|x|o");
+                for (int j = 0; j < height - 1; j++)
+                {
+                    Console.WriteLine("-+-+-");
+
+
+                }
+               
+            }
+            Console.WriteLine("o|x|o");
 
         }
 
