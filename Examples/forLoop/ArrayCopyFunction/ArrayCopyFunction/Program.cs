86f641f01e91dcc5faa00159fc0b5f52c77a7054
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-01-08 23:52:10 -0500

    Program copies elements from one array to another

---
+++ Examples/forLoop/ArrayCopyFunction/ArrayCopyFunction/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayCopyFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Program to copy elements from one array into another array
+            int[] arr1 = new int[] {7, 2, 5, 9, 11 };
+            int[] arr2 = new int[5];
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                arr2[i] = arr1[i];
+                Console.WriteLine("arr2[i] : " + arr2[i]);
+            }
+        }
+    }
+}
