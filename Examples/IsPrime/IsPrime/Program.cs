a2c85a87e5d871b17cab9f73f95d2c90e02dbd4c
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-01-10 14:34:54 -0500

    Program to check if a number is prime

--- Examples/IsPrime/IsPrime/Program.cs
+++ Examples/IsPrime/IsPrime/Program.cs
@@ -10,10 +10,32 @@ namespace IsPrime
     {
         static void Main(string[] args)
         {
-            for (int i = 2; i < 20; i++)
-            {
-
-            }
+            bool result = IsPrime(7);
+            Console.WriteLine(result);
         }
-    }
+         
+        static bool IsPrime(int num)
+        {
+            // find if a number is prime
+            // a number is prime if it is divisible by only itself and one then it is prime
+           
+            //set i = 2 since all numbers are divisible by 1
+            for( int i = 2; i < num; i++)
+            {
+                Console.WriteLine("Loop count: " + i);
+                if (num % i == 0)
+                {
+                    Console.WriteLine(i + " is not prime");
+                    return false;
+                }
+                else
+                {
+                    Console.WriteLine(num + " mod " + i + " is not zero");
+                }
+            }
+            return true;
+            
+        }
+        
+}
 }
