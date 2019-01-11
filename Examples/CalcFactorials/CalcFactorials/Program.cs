b15557766db9d5596a6412dd8b6edd30f620e0d7
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-01-11 00:09:24 -0500

    Program to find factors of a given number

--- Examples/CalcFactorials/CalcFactorials/Program.cs
+++ Examples/CalcFactorials/CalcFactorials/Program.cs
@@ -11,21 +11,30 @@ namespace CalcFactorials
         static void Main(string[] args)
         {
             int num = 12;
-            int result = getFactors(num);
+            int []result = GetFactors(num);
         }
 
-        public static int  getFactors(int num)
-        {// returns all factorials.
-            //var factors = [];
-            int[] factors = new int[] { };
-            for (var i = 1; i <= num; i++)
+        // Factors are what we can multiply to get the number Eg:1 x 6 = 6 so 1 and 6 are factors
+        static int[] GetFactors(int num)
+        {
+            int[] factors = new int[num + 1];
+
+            for (int i = 1; i <= num; i++)
             {
                 if (num % i == 0)
                 {
-                    factors.push(i);
+                    Console.WriteLine("Loop : " + i);
+                    Console.WriteLine(num + " % " + i + " == 0");
+                    factors[i] = i;
+
                 }
             }
+            foreach (int number in factors)
+            {
+                Console.WriteLine("Num : " + number);
+            }
             return factors;
+
         }
     }
 }
