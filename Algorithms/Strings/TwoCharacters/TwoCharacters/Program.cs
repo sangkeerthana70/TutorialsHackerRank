dc138e15efcfe0cb599cc1ea6567c180565fb671
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-01-14 18:09:37 -0500

    Modified the program.cs yet to implement the algo

--- Algorithms/Strings/TwoCharacters/TwoCharacters/Program.cs
+++ Algorithms/Strings/TwoCharacters/TwoCharacters/Program.cs
@@ -11,12 +11,10 @@ namespace TwoCharacters
         static void Main(string[] args)
         {
  
-            //string s = "beabeefeab";
-            //int result = alternate(s);
+            string s = "beabeefeab";
+            int result = alternate(s);
 
-            //string s = "aaabccddd";
-            string s = "baab";
-            string result1 = superReducedString(s);
+            
 
         }
 
@@ -27,40 +25,6 @@ namespace TwoCharacters
             return 0;
         }
 
-        // Complete the superReducedString function below.
-        static string superReducedString(string s)
-        {
-            string reducedString = "";
-            
-            for (int i = 0; i < s.Length; i++)
-            {               
-                if (i == s.Length - 1)
-                {
-                    break;
-
-                }
-                else
-                {
-                    if (s[i] == s[i + 1])
-                    {
-                        Console.WriteLine("same character");
-                        //Console.WriteLine("s[i] " + s[i]);
-                        //Console.WriteLine("s[i + 1] " + s[i + 1]);
-                        reducedString = System.Convert.ToString(s[i]) + System.Convert.ToString(s[i + 1]);
-                        //Console.WriteLine("new string " + newString);
-                         s = s.Replace(reducedString, "");
-                        Console.WriteLine("Reduced string after removing same characters " + s);
-                        
-                        if (s.Length == 0)
-                        {                           
-                            s = "Empty String";                           
-                        }
-                    }                   
-                }              
-            }
-            Console.WriteLine(s);
-            return s;
-        }
-
+        
     }
 }
