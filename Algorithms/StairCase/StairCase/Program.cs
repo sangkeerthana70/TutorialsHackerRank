2c90c57054baa90d0550dc7f89c04829c351e9c0
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-01-15 18:04:39 -0500

    Modified Program.cs to remove empty space

--- Algorithms/StairCase/StairCase/Program.cs
+++ Algorithms/StairCase/StairCase/Program.cs
@@ -18,22 +18,23 @@ namespace StairCase
         static void staircase(int n)
         {
 
-            for (int i = 0; i < (n); i++)
-            {
-                Console.WriteLine(" ");
-                for (int j = 0; j < n - i - 2; j++)
-                {
+            //for (int i = 0; i < (n); i++)
+            //{
+            //    Console.WriteLine(" ");
+            //    for (int j = 0; j < n - i - 2; j++)
+            //    {
 
-                    Console.Write(" ");
-                }
-                for (int k = 0; k <= i; k++)
-                {
-                    Console.Write("#");
-                }
-                //Console.WriteLine();
-            }
+            //        Console.Write(" ");
+            //    }
+            //    for (int k = 0; k <= i; k++)
+            //    {
+            //        Console.Write("#");
+            //    }
+            //    //Console.WriteLine();
+            //}
             //Console.WriteLine();
 
+            
             //for (int i = 0; i < n; i++)
             //{    
             //    for (int j = 0; j < n; j++)
@@ -43,8 +44,24 @@ namespace StairCase
             //    Console.WriteLine();
             //}
             //Console.WriteLine();
- 
 
+            //-------------------------------------------------------
+            for(int i = 0; i < n; i++)
+            {
+                //print spaces first that is less than n - i - 1 for each row
+                for (int j = 0; j < (n - i) - 1; j++)
+                {
+                    Console.Write("0");                  
+                }
+                // print # symbol in the same row after the space
+                for(int k = 0; k <= i; k++)
+                {
+                    Console.Write("#");
+                }
+                // move to the next line for the next row to be printed
+                Console.WriteLine(" ");
+            }            
+ 
         }
 
     }
