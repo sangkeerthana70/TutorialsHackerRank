14cc48b1c3ea745728db7c09b363da69bb78dd76
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-01-18 23:43:12 -0500

    Initial commit

---
+++ Algorithms/Strings/Anagram/Anagram/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anagram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = anagram(s);
+        }
+
+        // Complete the anagram function below.
+        static int anagram(string s)
+        {
+
+
+        }
+    }
+}
