bfed73dd2964a3b0a65aca7776a08a45be050cb9
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-01-20 14:21:40 -0500

    Program to calculate scores

--- Examples/NumberGuessingGame/NumberGuessingGame/Program.cs
+++ Examples/NumberGuessingGame/NumberGuessingGame/Program.cs
@@ -10,6 +10,7 @@ namespace NumberGuessingGame
     {
         static void Main(string[] args)
         {
+            /*
             int[] randomCode = new int[4];
             Random random = new Random();
             for (var i = 0; i < 4; i++)
@@ -21,35 +22,55 @@ namespace NumberGuessingGame
             {
                 Console.WriteLine(item);
             }
+            */
 
-            //int guess = 0;
-            //int score = 0;
-            //int g;
-            //while (guess != 6)
-            //{
-            //    Console.WriteLine("Guess a number between 1 and 5");
-            //    g = Convert.ToInt32(Console.ReadLine());
-            //    Console.WriteLine(g);
-
-            //    guess = g;
-            //    if (guess == 1)
-            //    {
-            //        score += 1;
-            //    }
-            //    else if(guess == 2)
-            //    {
-            //        score += 1;
-            //    }
-            //    else if(guess == 3)
-            //    {
-            //        score += 1;
-            //    }
-            //    else{
-            //        score += 1;
-            //    }
-            //    Console.WriteLine("score: " + score);
-            //}
-            
+            //------------------------------------------------------------
+            int[] gameCode = new int[4] { 1, 2, 3, 4 };
+            int input = 4321;
+            int[] result = CalculateScore(gameCode, input);
         }
+
+        public static int [] CalculateScore(int [] gameCode, int input)
+        {
+            int[] result = new int[2];
+            int CorrectNumbers = 0;
+            int CorrectPositions = 0;
+            //convert the input variable to a string
+            string s = input.ToString();
+            Console.WriteLine("s: " + s);
+            int currNum;
+            for(var i = 0; i < s.Length; i++)
+            {
+                Console.WriteLine("s[i] = " + s[i]);
+                currNum = Convert.ToInt32(s[i]) - 48;// 48 represents ascii of 0
+                Console.WriteLine("currNum =" + currNum);
+                for (var j = 0; j < gameCode.Length; j++)
+                {
+                    Console.WriteLine("gameCode[j] " + gameCode[j]);
+                    if(currNum == gameCode[j])
+                    {
+                        Console.WriteLine("Number matched");
+                        CorrectNumbers += 1;
+                        // check position of both numbers
+                        if(i == j)
+                        {
+                            Console.WriteLine("Position matched");
+                            CorrectPositions += 1;
+                        }
+                        break;
+
+                    }
+                }
+                result[0] = CorrectNumbers;
+                result[1] = CorrectPositions;
+            }
+            Console.WriteLine(string.Join(" ", CorrectNumbers, CorrectPositions));
+            return result;
+        }
+        
+
+
+
+
     }
 }
