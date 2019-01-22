474e05bd3a8804d93e421d4b995237ca2dadf11d
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-01-21 23:52:49 -0500

    Class and Objects

---
+++ ClassesAndObjectsExamples/IntroToObjects/IntroToObjects/Car.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroToObjects
+{
+    class Car
+    {
+        public string make { get; set; }
+        public string color { get; set; }
+        public string model { get; set; }
+        public int year { get; set; }
+
+
+        //constructor
+        public Car(string m, string c, string mod, int y)
+        {
+            this.make = m;
+            this.color = c;
+            this.model = mod;
+            this.year = y;
+
+        }
+        //empty constructor
+        public Car()
+        {
+            
+
+        }
+    }
+}
---
+++ ClassesAndObjectsExamples/IntroToObjects/IntroToObjects/Person.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroToObjects
+{
+    class Person
+    {
+       public string name { get; set; }
+       public string gender { get; set; }
+       public int age { get; set; }
+       public Car car { get; set; }
+
+        //constructor
+        public Person(string n, string g, int a)
+        {
+            this.name = n;
+            this.gender = g;
+            this.age = a;
+            this.car = new Car();
+            
+        }
+
+        public void printDetail()
+        {
+            Console.WriteLine(name + " is a " + gender + " and is " + age + " years old");
+
+            if (!String.IsNullOrEmpty(car.make))
+            {
+                Console.WriteLine(name + " has a " + car.make + " " + car.model + " that is the model " + car.year + " and is " + car.color + " In color");
+            }
+            else
+            {
+                Console.WriteLine(name + " does not have a car");
+            }
+            
+        }
+    }
+}
