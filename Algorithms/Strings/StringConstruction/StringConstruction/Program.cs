117109eab17e125bc613add25aecb319674a1331
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-01-21 22:07:35 -0500

    Usage of string builders

---
+++ Algorithms/Strings/StringConstruction/StringConstruction/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringConstruction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "abcd";
+            
+            int result = stringConstruction(s);
+        }
+
+        // Complete the stringConstruction function below.
+        static int stringConstruction(string s)
+        {
+            string p = "";
+            int cost = 0;
+            StringBuilder sb = new StringBuilder();
+ 
+            sb.Append(s);
+            
+            p += sb;
+            Console.WriteLine("p: " + p);
+
+            for(int i = 0; i < s.Length - 1; i++)
+            {
+                if(p[i] != p[i + 1])
+                {
+                   cost += 1;
+                }
+            }
+            Console.WriteLine("cost: " + cost);
+            return cost;
+
+        }
+    }
+}
