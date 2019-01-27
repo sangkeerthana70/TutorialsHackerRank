24c9839175c3d95bd2639d41b887a6dd4c043fef
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-01-26 21:49:52 -0500

    Usage of try catch

---
+++ Examples/CatchExceptionPractice/CatchExceptionPractice/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CatchExceptionPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Enter a number");
+            try
+            {
+                
+                int num = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("You have entered " + num);
+            }
+            catch(System.FormatException e)
+            {
+                Console.WriteLine("Invalid number supplied");
+                Console.WriteLine(e); 
+            }
+            finally
+            {
+                Console.WriteLine("In the finally block");
+            }
+
+
+        }
+    }
+}
