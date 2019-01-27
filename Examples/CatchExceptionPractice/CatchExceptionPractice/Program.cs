1deb04a740e94709a91cc66cb199e61a186197d1
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-01-26 21:53:15 -0500

    comments added

--- Examples/CatchExceptionPractice/CatchExceptionPractice/Program.cs
+++ Examples/CatchExceptionPractice/CatchExceptionPractice/Program.cs
@@ -18,11 +18,13 @@ namespace CatchExceptionPractice
                 int num = Convert.ToInt32(Console.ReadLine());
                 Console.WriteLine("You have entered " + num);
             }
+            //this code block gets executed when the user input is not a number and throws an exception
             catch(System.FormatException e)
             {
                 Console.WriteLine("Invalid number supplied");
                 Console.WriteLine(e); 
             }
+            // this block gets executed no matter whether there is an exception or not 
             finally
             {
                 Console.WriteLine("In the finally block");
