182ece603b2ee83cac6231d0c9c7503bc24cdf47
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-02-02 21:48:18 -0500

    Passing by value and passing by reference

---
+++ Examples/CSharpRefresh/CSharpRefresh/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpRefresh
+{
+    public class Person
+    {
+        public string name;
+        public int age;
+
+        public Person(string n, int a)
+        {
+            this.name = n;
+            this.age = a;
+        }
+    }
+}
---
+++ Examples/CSharpRefresh/CSharpRefresh/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpRefresh
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = " SuperBowl";
+
+            Console.WriteLine(word);
+            var result = CalcNumberOfCharactersInString(word);
+            Console.WriteLine(word);
+
+            //===========================================
+            Console.WriteLine();
+            Person person = new Person(" Anu", 70);
+            Console.WriteLine(person.name);
+            var result1 = CalcNumberOfCharactersInString(person);
+            Console.WriteLine(person.name);
+
+           
+        }
+
+        
+    }
+}
