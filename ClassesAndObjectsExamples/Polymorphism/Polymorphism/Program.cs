80ee298c95746a6a9738074a3561e3ff06d187b5
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-02-07 13:38:45 -0500

    Inheritance at work

---
+++ ClassesAndObjectsExamples/Polymorphism/Polymorphism/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Shape shape = new Shape();
+            Console.WriteLine(shape.Area());// returns 0 as it invokes the Area method inside the shape class
+
+            Rectangle rectangle = new Rectangle(15, 5);
+            Console.WriteLine(rectangle.Area());// returns 0 Even though rectangle does not have a method called area, it works because it inherited the method from the base class Shape. This is inheritence
+        }
+    }
+}
---
+++ ClassesAndObjectsExamples/Polymorphism/Polymorphism/Shape.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    public class Shape
+    {
+
+        public int Area()
+        {
+            return 0;
+        }
+    }
+}
