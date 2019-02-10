2b81b8ab21d04ff327f64bceeb02b929788adb88
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-02-09 23:32:52 -0500

    Initial commit

---
+++ Algorithms/TwoStrings/TwoStrings/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "a";
+            string b = "art";
+            string result = twoStrings(a, b);
+
+        }
+
+        // Complete the twoStrings function below.
+        static string twoStrings(string s1, string s2)
+        {
+
+
+        }
+    }
+}
