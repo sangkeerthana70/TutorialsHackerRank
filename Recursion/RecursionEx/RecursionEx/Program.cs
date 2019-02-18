fc0f13fd07b934ad49499377da70a88a149cd419
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-02-18 13:12:32 -0500

    calculate sum of numbers in a range using recursion

---
+++ Recursion/RecursionEx/RecursionEx/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecursionEx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 1;
+            int m = 10;
+            int result = CalculateSumRecursively(n, m);
+            Console.WriteLine(result);
+        }
+
+        public static int CalculateSumRecursively(int n, int m)
+        {
+            int sum = n;
+
+            if (n < m)
+            {
+                n++;
+                return sum += CalculateSumRecursively(n, m);
+            }
+
+            return sum;
+        }
+    }
+}
