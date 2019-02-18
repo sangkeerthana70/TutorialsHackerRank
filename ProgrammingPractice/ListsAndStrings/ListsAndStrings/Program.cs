c0c71909c84290db52955dbd56048b4a0cf03af1
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-02-18 16:05:11 -0500

    Find largest element in a list

---
+++ ProgrammingPractice/ListsAndStrings/ListsAndStrings/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListsAndStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // function that returns the largest element in a list.
+            List<int> numbers = new List<int> { 11, 13, 15, 5, 7, 9 };
+
+            int maxValue = int.MinValue;
+            for(var i = 0; i < numbers.Count; i++)
+            {
+                if(numbers[i] > maxValue)
+                {
+                    maxValue = numbers[i];
+                }
+            }
+            Console.WriteLine("max value: " + maxValue);
+
+        }
+    }
+}
