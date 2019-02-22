51e050970783e8d5b7c2b8562de00303286ddc28
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-02-22 00:11:48 -0500

    More examples on accessing elements in 2-D array

--- Examples/2DArrays/2DArrays/Program.cs
+++ Examples/2DArrays/2DArrays/Program.cs
@@ -24,6 +24,22 @@ namespace _2DArrays
             Console.WriteLine(elem);
             Console.WriteLine(val);
 
+
+            /* an array with 5 rows and 2 columns*/
+            int[,] b = new int[5, 2] { { 0, 0 }, { 1, 2 }, { 2, 4 }, { 3, 6 }, { 4, 8 } };
+            int i, j;
+
+            /* output each array element's value */
+            for (i = 0; i < 5; i++)
+            {
+
+                for (j = 0; j < 2; j++)
+                {
+                    Console.WriteLine("a[{0},{1}] = {2}", i, j, b[i, j]);
+                }
+            }
+            Console.ReadKey();
+
         }
     }
 }
