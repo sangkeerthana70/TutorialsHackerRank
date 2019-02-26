cb15158dfe23327d091d26adad3c3f24b2a43bb9
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-02-26 13:55:14 -0500

    Program to find the max sum of the triangle in a 2D array

--- 2DArrays/FindMaxSumOfTriangleIn2DArray/FindMaxSumOfTriangleIn2DArray/Program.cs
+++ 2DArrays/FindMaxSumOfTriangleIn2DArray/FindMaxSumOfTriangleIn2DArray/Program.cs
@@ -29,6 +29,7 @@ namespace FindMaxSumOfTriangleIn2DArray
                 { 0, 0, 0, 0, 0, 0 }
             };
 
+            /*
             // access the tip of the triangle
             // outer loop to access the rows
             for(var i = 0; i < a.GetLength(0) - 1; i++)
@@ -44,7 +45,35 @@ namespace FindMaxSumOfTriangleIn2DArray
                 }
                 Console.WriteLine();
             }
-            
+            */
+
+            // Calculate the max sum of the triangle in the 2D array
+            int triangleSum = 0;
+
+            int maxValue = int.MinValue;
+            for (var i = 0; i < a.GetLength(0) - 1; i++)
+            {
+                Console.WriteLine("i: " + i);
+                for (var j = 1; j < a.GetLength(1) - 1; j++)
+                {
+                    Console.WriteLine("j: " + j);
+                    Console.WriteLine("a[i, j]: " + a[i, j]);
+                    triangleSum = a[i, j];
+                    Console.WriteLine("a[i + 1, j-1]:" + a[i + 1, j - 1]);
+                    Console.WriteLine("a[i + 1, j]" + a[i + 1, j]);
+                    Console.WriteLine("a[i + 1, j + 1]" + a[i + 1, j + 1]);
+                    triangleSum += a[i + 1, j - 1] + a[i + 1, j] + a[i + 1, j + 1];
+                    Console.WriteLine("triangleSum: " + triangleSum);
+                    if (triangleSum > maxValue)
+                    {
+                        maxValue = triangleSum;
+                    }
+                }
+                //Console.WriteLine("triangleSum: " + triangleSum);
+            }
+            Console.WriteLine("maxValue: " + maxValue);
+
+
         }
     }
 }
