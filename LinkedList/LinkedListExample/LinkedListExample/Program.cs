22e46cc97a61d45f1131cfd16e5fbefc34175d2e
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-03-01 23:34:31 -0500

    Example

---
+++ LinkedList/LinkedListExample/LinkedListExample/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<int> myLinkedList = new LinkedList<int>();
+
+            // Add a node with data=25 to back of the (empty) list
+            myLinkedList.AddLast(25);
+
+            myLinkedList.AddLast(30);
+            // Insert a node at front of the list
+            myLinkedList.AddFirst(20);
+            //Insert a node with data = 30 at back of the list
+            myLinkedList.AddLast(35);
+            // // Insert a node with data="15" at index 2
+
+            var currentNode = myLinkedList.First;
+            Console.WriteLine("curr node: " + currentNode.Value);
+            while(currentNode.Next != null)
+            {
+                currentNode = currentNode.Next;
+                Console.WriteLine("currentNode value: " + currentNode.Value);
+            }
+            
+        }
+    }
+}
