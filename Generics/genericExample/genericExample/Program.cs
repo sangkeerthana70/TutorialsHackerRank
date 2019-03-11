259a68804415b608881cc093d00325b0ece7258f
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-03-11 00:23:48 -0400

    Example added

--- Generics/genericExample/genericExample/Program.cs
+++ Generics/genericExample/genericExample/Program.cs
@@ -10,6 +10,36 @@ namespace genericExample
     {
         static void Main(string[] args)
         {
+            bool Equal = Calculator.AreEqual<int>(20, 20);
+            
+            if (Equal)
+            {
+                Console.WriteLine("Values are Equal");
+            }
+            else
+            {
+                Console.WriteLine("Values are not equal");
+            }
+
+            bool stringEqual = Calculator.AreEqual<string>("hello", "world");
+            if (stringEqual)
+            {
+                Console.WriteLine("String Values are Equal");
+            }
+            else
+            {
+                Console.WriteLine("String Values are not equal");
+            }
         }
+
+        public class Calculator
+        {
+            public static bool AreEqual<T>(T value1, T value2)
+            {
+                return value1.Equals(value2);
+            }
+        }
+
+
     }
 }
