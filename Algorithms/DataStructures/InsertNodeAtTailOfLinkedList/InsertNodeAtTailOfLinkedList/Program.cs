f9e0964c6a622598c8ce640e5f8e06916b5c11e4
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-03-13 14:41:47 -0400

    Initial commit

---
+++ Algorithms/DataStructures/InsertNodeAtTailOfLinkedList/InsertNodeAtTailOfLinkedList/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsertNodeAtTailOfLinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SinglyLinkedList llist = new SinglyLinkedList();
+
+            int llistCount = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < llistCount; i++)
+            {
+                int llistItem = Convert.ToInt32(Console.ReadLine());
+                SinglyLinkedListNode llist_head = insertNodeAtTail(llist.head, llistItem);
+                llist.head = llist_head;
+
+            }
+            PrintSinglyLinkedList(llist.head);
+
+
+
+        }
+
+        class SinglyLinkedListNode
+        {
+            public int data;
+            public SinglyLinkedListNode next;
+
+            public SinglyLinkedListNode(int nodeData)
+            {
+                this.data = nodeData;
+                this.next = null;
+            }
+        }
+
+        class SinglyLinkedList
+        {
+            public SinglyLinkedListNode head;
+
+            public SinglyLinkedList()
+            {
+                this.head = null;
+            }
+
+        }
+
+        static void PrintSinglyLinkedList(SinglyLinkedListNode node)
+        {
+            while (node != null)
+            {
+                Console.WriteLine(node.data);
+
+                node = node.next;
+
+                if (node != null)
+                {
+                    Console.WriteLine(node.data);
+                }
+            }
+        }
+
+        // Complete the insertNodeAtTail function below.
+
+        /*
+         * For your reference:
+         *
+         * SinglyLinkedListNode {
+         *     int data;
+         *     SinglyLinkedListNode next;
+         * }
+         *
+         */
+        static SinglyLinkedListNode insertNodeAtTail(SinglyLinkedListNode head, int data)
+        {
+
+
+        }
+
+
+    }
+}
