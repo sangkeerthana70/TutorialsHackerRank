b85077e8e9c115c310c12f4056837cc3385e3fa8
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-03-13 14:32:52 -0400

    Print elements in a Linked List

---
+++ Algorithms/DataStructures/PrintElementsOfLinkedList/PrintElementsOfLinkedList/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintElementsOfLinkedList
+{
+    class Program
+    {
+        class SinglyLinkedListNode
+        {
+            public int data;
+            public SinglyLinkedListNode next;
+
+            public SinglyLinkedListNode(int nodeData)
+            {
+                this.data = nodeData;
+                this.next = null;
+            }
+        }
+
+        class SinglyLinkedList
+        {
+            public SinglyLinkedListNode head;
+            public SinglyLinkedListNode tail;
+
+            public SinglyLinkedList()
+            {
+                this.head = null;
+                this.tail = null;
+            }
+
+            public void InsertNode(int nodeData)
+            {
+                SinglyLinkedListNode node = new SinglyLinkedListNode(nodeData);
+
+                if (this.head == null)
+                {
+                    this.head = node;
+                }
+                else
+                {
+                    this.tail.next = node;
+                }
+
+                this.tail = node;
+
+            }
+
+            static void printLinkedList(SinglyLinkedListNode head)
+            {
+               
+                var currNode = head;
+                Console.WriteLine(currNode.data);
+                while (currNode != null)
+                {
+                    if(currNode.next == null)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        currNode = currNode.next;
+                        Console.WriteLine(currNode.data);
+                    }
+                    
+                    
+                }
+ 
+
+            }
+
+
+            static void Main(string[] args)
+            {
+                SinglyLinkedList llist = new SinglyLinkedList();
+
+                int llistCount = Convert.ToInt32(Console.ReadLine());
+
+                for (int i = 0; i < llistCount; i++)
+                {
+                    int llistItem = Convert.ToInt32(Console.ReadLine());
+                    llist.InsertNode(llistItem);
+                }
+
+                printLinkedList(llist.head);
+
+            }
+        }
+
+    }
+}
+    
+
