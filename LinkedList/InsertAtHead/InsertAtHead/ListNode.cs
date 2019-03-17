ea1212c258f8a02270a7c17c19ed012211bff262
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-03-16 20:08:21 -0400

    Created a Linked list function to create a linked list first

---
+++ LinkedList/InsertAtHead/InsertAtHead/ListNode.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsertAtHead
+{
+    //Definition for singly-linked list:
+    public class ListNode<T>
+    {
+        public T value { get; set; }
+
+        public ListNode<T> next { get; set; }
+    }
+}
---
+++ LinkedList/InsertAtHead/InsertAtHead/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsertAtHead
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> intList = new List<int> { 3, 1, 2, 3, 4, 5 };
+
+            var linkedList = CreateLinkedList(intList);
+        }
+
+        public static ListNode<int> CreateLinkedList(List<int> intList)
+        {
+            ListNode<int> head = new ListNode<int>();
+
+            var currNode = head;
+
+            for(var i = 0; i < intList.Count; i++)
+            {
+                currNode.value = intList[i];
+               
+                if(i < intList.Count -1)
+                {
+                    // create next node 
+                    currNode.next = new ListNode<int>();
+                    Console.WriteLine("currNode: " + currNode.value);
+                    
+                    currNode = currNode.next;
+                    
+
+                }
+                
+            }
+            return head;
+        }
+
+
+    }
+
+    
+}
