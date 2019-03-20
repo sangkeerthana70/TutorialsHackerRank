b0c22f172e3d4c4df1d3d41a7036fb231f38f268
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-03-19 23:06:52 -0400

    Program to return largest element in a list

--- ProgrammingPractice/ListsAndStrings/ListsAndStrings/Program.cs
+++ ProgrammingPractice/ListsAndStrings/ListsAndStrings/Program.cs
@@ -10,6 +10,7 @@ namespace ListsAndStrings
     {
         static void Main(string[] args)
         {
+            /*
             // function that returns the largest element in a list.
             List<int> numbers = new List<int> { 11, 13, 15, 5, 7, 9 };
 
@@ -22,6 +23,37 @@ namespace ListsAndStrings
                 }
             }
             Console.WriteLine("max value: " + maxValue);
+            */
+
+            int num = 12;
+            int[] result = GetFactors(num);
+            Console.WriteLine(String.Join(",", result));
+        }
+
+        static int[] GetFactors(int num)
+        {
+            int[] factors = new int[num + 1];
+
+            for (int i = 1; i <= num; i++)
+            {
+                if (num % i == 0)
+                {
+                    //Console.WriteLine("Loop : " + i);
+                    //Console.WriteLine(num + " % " + i + " == 0");
+                    factors[i] = i;
+
+                }
+                //else
+                //{
+                    //Console.WriteLine("Loop : " + i);
+                    //Console.WriteLine(num + " % " + i + " != 0");
+                //}
+            }
+            //foreach (int number in factors)
+            //{
+            //    Console.WriteLine("Num : " + number);
+            //}
+            return factors;
 
         }
     }
