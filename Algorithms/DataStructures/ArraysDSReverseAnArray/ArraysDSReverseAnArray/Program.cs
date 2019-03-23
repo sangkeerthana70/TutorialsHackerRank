246829214c10bf0ac3b51191869504cc8afa994f
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-03-23 19:07:41 -0400

    reverse an array

---
+++ Algorithms/DataStructures/ArraysDSReverseAnArray/ArraysDSReverseAnArray/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraysDSReverseAnArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 1, 4, 3, 2 };
+            int[] result = reverseArray(arr);
+            Console.WriteLine(String.Join(",", result));
+        }
+
+        // Complete the reverseArray function below.
+        static int[] reverseArray(int[] a)
+        {
+            int length = a.Length -1;
+            int[] reverseArray = new int[a.Length];
+            for (int i = length; i >= 0; i--)
+            {
+                Console.WriteLine("i = " + i);
+                Console.WriteLine("arr1[i] = " + a[i]);
+                Console.WriteLine();
+                reverseArray[length - i] = a[i];
+                Console.WriteLine("reverseArray[length - i] : " + reverseArray[length-i]);
+            }
+            return reverseArray;
+        }
+    }
+}
