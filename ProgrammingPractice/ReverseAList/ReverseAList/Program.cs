80baef1e04c5c0e7838fe6fa8580a8bfa1ac5a33
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-03-23 23:51:46 -0400

    Initial commit

---
+++ ProgrammingPractice/ReverseAList/ReverseAList/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseAList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> myList = new List<int>() { 5, 3, 4, 2, 1 };
+            for(var i = 0; i < myList.Count; i++)
+            {
+
+            }
+        }
+    }
+}
