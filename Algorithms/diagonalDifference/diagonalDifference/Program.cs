c0f059baac5c6141aa147cabeeefa1d906731922
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-03-26 15:13:41 -0400

    expected outcome

--- Algorithms/diagonalDifference/diagonalDifference/Program.cs
+++ Algorithms/diagonalDifference/diagonalDifference/Program.cs
@@ -10,10 +10,11 @@ namespace diagonalDifference
     {
         static void Main(string[] args)
         {
-            int[][] squareMatrix = new int[3][];
-            squareMatrix[0] = new int[] { 11, 2, 4 };
-            squareMatrix[1] = new int[] { 4, 5, 6 };
-            squareMatrix[2] = new int[] { 10, 8, -12 };
+            int[][] squareMatrix = new int[4][];
+            squareMatrix[0] = new int[] { -1, 1, - 7, - 8 };
+            squareMatrix[1] = new int[] { -10, - 8, - 5, - 2 };
+            squareMatrix[2] = new int[] { 0, 9, 7, - 1 };
+            squareMatrix[3] = new int[] { 4, 4, - 2, 1 };
 
             int result = diagonalDifference(squareMatrix);
             Console.WriteLine("diagonalDifference : " + result);
@@ -25,47 +26,48 @@ namespace diagonalDifference
             int lToRDiagDiff = 0;
             int rToLDiagDiff = 0;
             int diagonalDifference = 0;
-            
-            for(var j = 0; j < arr.GetLength(0); j++)
+            int len = arr.GetLength(0);
+
+            for (var i = 0; i <= len -1; i++)
             {
-                int len = arr.GetLength(0);
-                //Console.WriteLine("len: " + len);
-                //Console.WriteLine("j is: " + j);
-                int[] innerArray = arr[j];
-                //Console.WriteLine(String.Join(" ", innerArray));
-                for(var i = 0; i <= 1; i++)          
+                
+                Console.WriteLine("len: " + len);
+                Console.WriteLine("i is: " + i);
+                int[] innerArray = arr[i];
+                Console.WriteLine(String.Join(" ", innerArray));
+                for(var j = 0; j <= innerArray.Length; j++)          
                 {
                     // to loop through the innerArray only once use this condition
                     if(i == j)
                     {
-                        //Console.WriteLine("i is: " + i);
-                        //Console.WriteLine("j is: " + j);
-                        //Console.WriteLine("arr[j][i]: " + arr[j][i]);
-                        lToRDiagDiff = arr[j][i];
-                        //Console.WriteLine("arr[j + 1][i + 1]: " + arr[j + 1][i + 1]);
-                        lToRDiagDiff += arr[j + 1][i + 1];
-
-                        lToRDiagDiff += arr[j + 2][i + 2];
-                        ////Console.WriteLine("arr[j + 2][i + 2]: " + arr[j + 2][i + 2]);
-                        Console.WriteLine("lToRDiagDiff: " + lToRDiagDiff);
-                        //Console.WriteLine("---------------------------------------------");
-                        rToLDiagDiff = arr[j][len -1];
-                        //Console.WriteLine("arr[j][len -1]: " + arr[j][len - 1]);
-                        rToLDiagDiff += arr[j + 1][i + 1];
-                        //Console.WriteLine("arr[j + 1][i + 1]: " + arr[j + 1][i + 1]);
-                        rToLDiagDiff += arr[j+2][i];
-                        //Console.WriteLine("arr[j+2][i]: " + arr[j+2][i]);
-                        //Console.WriteLine("rToLDiagDiff: " + rToLDiagDiff);
+                        Console.WriteLine("i is: " + i);
+                        Console.WriteLine("j is: " + j);
+                        lToRDiagDiff = arr[i][j];
+                        //Console.WriteLine(arr[i][j]);
+                        lToRDiagDiff += arr[i + 1][j + 1];
+                        //Console.WriteLine(arr[i + 1][j + 1]);
+                        lToRDiagDiff += arr[i + 2][j + 2];                       
+                        //Console.WriteLine(arr[i + 2][j + 2]);
+                        lToRDiagDiff += arr[i + 3][j + 3];
+                        //Console.WriteLine(arr[i + 3][j + 3]);
+                        Console.WriteLine(lToRDiagDiff);
+                        Console.WriteLine("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXx");
+                        rToLDiagDiff = arr[i][len-1];
+                        Console.WriteLine("ll" + arr[i][len-1]);
+                        rToLDiagDiff += arr[i + 1][len - 2];
+                        Console.WriteLine(arr[i + 1][len - 2]);
+                        rToLDiagDiff += arr[i + 2][len - 3];
+                        Console.WriteLine(arr[i + 2][len - 3]);
+                        rToLDiagDiff += arr[i + 3][len - 4];
+                        Console.WriteLine(arr[i + 3][len - 4]);
+                        Console.WriteLine(rToLDiagDiff);
                     }
                     else if(i != j)
                     {
                         break;
-                    }
-                    diagonalDifference = Math.Abs(lToRDiagDiff - rToLDiagDiff);
-
-                   
-                    
+                    }                   
                 }
+                diagonalDifference = Math.Abs(lToRDiagDiff - rToLDiagDiff);
             }
 
             return diagonalDifference;
