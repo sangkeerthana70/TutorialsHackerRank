67e62ed9e4748bd5a1f43625a1b5b6f076443ca8
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-03-27 22:57:29 -0400

    Initial commit

---
+++ Algorithms/MigratoryBirds/MigratoryBirds/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MigratoryBirds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> arr = new List<int> { 1, 4, 4, 4, 5, 3 };
+            int result = migratoryBirds(arr);
+        }
+
+        // Complete the migratoryBirds function below.
+        static int migratoryBirds(List<int> arr)
+        {
+            Dictionary<int, int> birdCount = new Dictionary<int, int>();
+            for (var i = 0; i < arr.Count; i++)
+            {
+                
+                birdCount.Add(arr[i], 1);
+            }
+            
+
+        }
+    }
+}
