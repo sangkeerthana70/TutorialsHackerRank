79e4ad284873db396a4b208879d3ac11fab46113
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-03-27 23:13:39 -0400

    working solution

--- Algorithms/MigratoryBirds/MigratoryBirds/Program.cs
+++ Algorithms/MigratoryBirds/MigratoryBirds/Program.cs
@@ -17,14 +17,41 @@ namespace MigratoryBirds
         // Complete the migratoryBirds function below.
         static int migratoryBirds(List<int> arr)
         {
-            Dictionary<int, int> birdCount = new Dictionary<int, int>();
+            Dictionary<int, int> birds = new Dictionary<int, int>();
+            int maxValue = int.MinValue;
+            int refKey = 0;
+
             for (var i = 0; i < arr.Count; i++)
             {
+                if (birds.ContainsKey(arr[i]))
+                {
+                    // if the key already exists increment its value by 1
+                    birds[arr[i]] += 1;
+                }
+                else
+                {
+                    birds.Add(arr[i], 1);
+                }
                 
-                birdCount.Add(arr[i], 1);
             }
-            
+            foreach(var bird in birds)
+            {
+                Console.WriteLine("Key = {0} value = {1}", bird.Key, bird.Value);
+                // find the maximum value in the dictionary
+                if (bird.Value > maxValue)
+                {
+                    maxValue = bird.Value;
+                    refKey = bird.Key;
+                    //Console.WriteLine("Key: " + refKey);
 
+                }
+            }
+            // return the number with the most occurrance
+            Console.WriteLine("The most spotted bird is = {0}  ", refKey);
+            return refKey;
+
+
+            
         }
     }
 }
