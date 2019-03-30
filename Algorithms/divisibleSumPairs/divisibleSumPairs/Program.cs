fd6736078b670b90ce6a091d81885938a603dad1
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-03-29 23:46:32 -0400

    passed all test cases

--- Algorithms/divisibleSumPairs/divisibleSumPairs/Program.cs
+++ Algorithms/divisibleSumPairs/divisibleSumPairs/Program.cs
@@ -10,8 +10,8 @@ namespace divisibleSumPairs
     {
         static void Main(string[] args)
         {
-            int k = 3;
-            int[] arr = new int[] { 1, 3, 2, 6, 1, 2, };
+            int k = 7;
+            int[] arr = new int[] { 36, 46, 25, 97, 57, 14, 21, 50, 75, 58, 54, 32, 73, 11, 36, 22, 95, 46, 54, 61 };
             int result = divisibleSumPairs(k, arr);
             Console.WriteLine("result: " + result);
         }
@@ -22,17 +22,17 @@ namespace divisibleSumPairs
             int divisiblePairCount = 0;
             for (int i = 0; i < arr.Length; i++)
             {
-                Console.WriteLine("i is : " + i);
-                Console.WriteLine("arr[i]: " + arr[i]);
+                //Console.WriteLine("i is : " + i);
+                //Console.WriteLine("arr[i]: " + arr[i]);
                 for(int j = i+1; j < arr.Length; j++)
                 {
-                    Console.WriteLine("j is: " + j);
-                    Console.WriteLine("arr[i] and arr[j] : " + arr[i] + " " + arr[j]);
+                    //Console.WriteLine("j is: " + j);
+                    //Console.WriteLine("arr[i] and arr[j] : " + arr[i] + " " + arr[j]);
                     if((arr[i] + arr[j]) % k == 0)
                     {
-                        Console.WriteLine("pairs divisible by k: " + arr[i] + " " + arr[j]);
+                       // Console.WriteLine("pairs divisible by k: " + arr[i] + " " + arr[j]);
                         divisiblePairCount += 1;
-                        Console.WriteLine("divisiblePairCount: " + divisiblePairCount);
+                        //Console.WriteLine("divisiblePairCount: " + divisiblePairCount);
                     }
                 }
             }
