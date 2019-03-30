9736f53124445852675e6e5f5cbdee3c6c3966cc
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-03-29 23:39:55 -0400

    working solution

--- Algorithms/divisibleSumPairs/divisibleSumPairs/Program.cs
+++ Algorithms/divisibleSumPairs/divisibleSumPairs/Program.cs
@@ -13,12 +13,30 @@ namespace divisibleSumPairs
             int k = 3;
             int[] arr = new int[] { 1, 3, 2, 6, 1, 2, };
             int result = divisibleSumPairs(k, arr);
+            Console.WriteLine("result: " + result);
         }
 
         // Complete the divisibleSumPairs function below.
         static int divisibleSumPairs(int k, int[] arr)
         {
-
+            int divisiblePairCount = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine("i is : " + i);
+                Console.WriteLine("arr[i]: " + arr[i]);
+                for(int j = i+1; j < arr.Length; j++)
+                {
+                    Console.WriteLine("j is: " + j);
+                    Console.WriteLine("arr[i] and arr[j] : " + arr[i] + " " + arr[j]);
+                    if((arr[i] + arr[j]) % k == 0)
+                    {
+                        Console.WriteLine("pairs divisible by k: " + arr[i] + " " + arr[j]);
+                        divisiblePairCount += 1;
+                        Console.WriteLine("divisiblePairCount: " + divisiblePairCount);
+                    }
+                }
+            }
+            return divisiblePairCount;
 
         }
     }
