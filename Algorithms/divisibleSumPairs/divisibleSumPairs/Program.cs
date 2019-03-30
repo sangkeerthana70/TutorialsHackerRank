b2a2ad1b9ff6e7d72391d0f9a320a49f4a24a73a
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-03-29 22:55:33 -0400

    initial commit

---
+++ Algorithms/divisibleSumPairs/divisibleSumPairs/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace divisibleSumPairs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int k = 3;
+            int[] arr = new int[] { 1, 3, 2, 6, 1, 2, };
+            int result = divisibleSumPairs(k, arr);
+        }
+
+        // Complete the divisibleSumPairs function below.
+        static int divisibleSumPairs(int k, int[] arr)
+        {
+
+
+        }
+    }
+}
