4bae3876dd0a8069784b609079e2d58b9210b62d
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-03-31 12:41:37 -0400

    Find the max height of a BST implemented

--- BinarySearchTree/BinarySearchTree/Program.cs
+++ BinarySearchTree/BinarySearchTree/Program.cs
@@ -11,19 +11,19 @@ namespace BinarySearchTree
         static void Main(string[] args)
         {
             Node root = new Node(3);
-            Console.WriteLine("root.data: " + root.data);
+            //Console.WriteLine("root.data: " + root.data);
             root.left = new Node(2);
-            Console.WriteLine("root.left: " + root.left.data);
+            //Console.WriteLine("root.left: " + root.left.data);
             root.right = new Node(5);
-            Console.WriteLine("root.right: " + root.right.data);
+            //Console.WriteLine("root.right: " + root.right.data);
             root.left.left = new Node(1);
-            Console.WriteLine("root.left.left: " + root.left.left.data);
+            //Console.WriteLine("root.left.left: " + root.left.left.data);
             root.right.left = new Node(4);
-            Console.WriteLine("root.right.left: " + root.right.left.data);
+            //Console.WriteLine("root.right.left: " + root.right.left.data);
             root.right.right = new Node(6);
-            Console.WriteLine("root.right.right: " + root.right.right.data);
+            //Console.WriteLine("root.right.right: " + root.right.right.data);
             root.right.right.right = new Node(7);
-            Console.WriteLine("root.right.right.right: " + root.right.right.right.data);
+            //Console.WriteLine("root.right.right.right: " + root.right.right.right.data);
             
 
             int result = getHeight(root);
@@ -35,10 +35,28 @@ namespace BinarySearchTree
         {
             //Write your code here
             int height = 0;
-            if (root == null) return 0;
             
-            height = (1 + Math.Max(getHeight(root.left), getHeight(root.right)));
-            Console.WriteLine("height for each recursion call: " + height);
+            if (root == null) return -1;
+
+            int left = getHeight(root.left);
+            Console.WriteLine("left: " + root.left);
+            int right = getHeight(root.right);
+            Console.WriteLine("right: " + root.right);
+
+            if (left > right)
+            {
+                 height = 1 + left;
+                Console.WriteLine("If, height: " + height);
+            }
+            else
+            {
+                 height = 1 + right;
+                Console.WriteLine("Else, height: " + height);
+            }
+
+
+
+            //return Math.Max(left, right);
             return height;
         }
     }
