6dc8a0698d1da7f4027528b101af81e1bc5b6438
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-03-31 02:39:33 -0400

    Initial commit

---
+++ Algorithms/BonAppetit/BonAppetit/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BonAppetit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int index = 1;
+            List<int> billList = new List<int> { 3, 10, 2, 9 };
+            int amountCharged = 7;
+
+            bonAppetit(billList, index, amountCharged);
+        }
+
+        // Complete the bonAppetit function below.
+        static void bonAppetit(List<int> bill, int k, int b)
+        {
+            //Console.WriteLine("b is "  + b);
+            int billTotal = 0;
+            int annaOws = 0;
+            
+            //Console.WriteLine("bill[k] " + bill[k]);
+            for (int i = 0; i < bill.Count; i++)
+            {
+                billTotal += bill[i];
+                //Console.WriteLine("billTotal : " + billTotal);
+            }
+            annaOws = (billTotal - bill[k]) / 2;
+            //Console.WriteLine("annaOws: " + annaOws);
+            if(b == annaOws)
+            {
+               // Console.WriteLine("inside if");
+                Console.WriteLine("Bon Appetit");
+            }
+            else
+            {
+                //Console.WriteLine("in else");
+                int amtBrianOwsAnna = b - annaOws;
+                //Console.WriteLine("amtBrianOwsAnna: " + amtBrianOwsAnna);
+            }
+
+        }
+
+    }
+}
