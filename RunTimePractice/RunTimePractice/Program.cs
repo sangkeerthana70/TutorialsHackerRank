e35f2bb39765711c0986dfa67355ba74aa7f8428
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-04-10 15:14:21 -0400

    A simple program to calculate the runtime of an algorithm

---
+++ RunTimePractice/RunTimePractice/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RunTimePractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string S = "abca";
+            char c = 'a';
+            long startTime = DateTime.Now.Millisecond;
+            int res = findRepeatedOccuranceOfACharInString(S, c);
+            Console.WriteLine("res: " + res);
+            long endTime = DateTime.Now.Millisecond;
+            long duration = endTime - startTime;
+            Console.WriteLine("Time taken in milliseconds: " + duration);
+        }
+
+        public static int findRepeatedOccuranceOfACharInString(string S, char c)
+        {
+            int occurance = 0;
+            for(int i = 0; i < S.Length; i++)
+            {
+                //Console.WriteLine(S.Contains(c));
+                if (S.Contains(c))
+                {
+                    occurance++;
+                }
+            }
+            return occurance;
+        }
+    }
+}
