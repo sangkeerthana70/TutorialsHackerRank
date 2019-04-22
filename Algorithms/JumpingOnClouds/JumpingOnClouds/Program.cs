c2ad1a9c4af15392d6a9e193ddae6d1c77e4109a
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-04-21 23:56:32 -0400

    Initial commit

--- Algorithms/JumpingOnClouds/JumpingOnClouds/Program.cs
+++ Algorithms/JumpingOnClouds/JumpingOnClouds/Program.cs
@@ -10,6 +10,31 @@ namespace JumpingOnClouds
     {
         static void Main(string[] args)
         {
+            int[] cloudArray = new int[] { 0, 0, 1, 0, 0, 1, 0 };
+            FindNumberOfCloudsJumped(cloudArray);
+        }
+
+        static int FindNumberOfCloudsJumped(int[] cloudArray)
+        {
+            int counter = 0;
+            for(int i = 0; i < cloudArray.Length -1; i++)
+            {
+                if(cloudArray[i] == 0)
+                {
+                    if(cloudArray[i + 1] == 0)
+                    {
+                        counter += 1;
+                        continue;
+                    }
+                    else
+                    {
+                        counter = 0;
+                        continue;
+                    }
+                }
+            }
+            Console.WriteLine("counter check: " + counter);
+            return counter;
         }
     }
 }
